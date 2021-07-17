b4b2c1d7f04070bfdf2e79d98fbacef344acca67
Dylan Koss <dylanhkoss@gmail.com>
2021-07-17 18:33:23 -0500

    Edit budget button is working!

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ import './App.css';
 function App() {
   const [budgetAmount, setBudgetAmount] = useState(0);
   const [submitted, setSubmit] = useState(false);
+  const [editBudget, setEditBudget] = useState(false);
+
+  const showEditBudget = (e) => {
+    setEditBudget(true);
+  }
 
   return (
     <div className="App">
       <Header budgetAmount={budgetAmount} />
-      {!submitted && <AddBudget setBudgetAmount={setBudgetAmount} setSubmit={setSubmit} />}
-      <div className='addExpense'>Add Expense</div>
+      {!submitted && <AddBudget setBudgetAmount={setBudgetAmount} setSubmit={setSubmit} setEditBudget={setEditBudget} />}
+      {editBudget && <AddBudget setBudgetAmount={setBudgetAmount} setSubmit={setSubmit} setEditBudget={setEditBudget} />}
+      {submitted && <div className='buttonUI'>Add Expense</div>}
+      {submitted && <div className='buttonUI' onClick={showEditBudget}>Edit Budget</div>}
     </div>
   );
 }
--- src/components/AddBudget.js
+++ src/components/AddBudget.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const AddBudget = ({ setBudgetAmount, setSubmit }) => {
+const AddBudget = ({ setBudgetAmount, setSubmit, setEditBudget }) => {
     return (
         <div className='formWrapper'>
             <form onSubmit={(e) => {
                 e.preventDefault();
                 setSubmit(true);
+                setEditBudget(false);
             }}>
                 <label for='addbudget'>Set your budget for the week</label>
                 <input type='number' name='addbudget' onChange={(e) => setBudgetAmount(e.target.value)} />
